c87fac5d3a0d55db2763f25dc4951c38da04aaec
Andrii <singapurchik666@gmail.com>
2025-11-21 00:18:52 +0100

    fixed input on mobile

--- Assets/_MergeTest/Scripts/Core/Player/Player Input/PlayerInput.cs
+++ Assets/_MergeTest/Scripts/Core/Player/Player Input/PlayerInput.cs
@@ -1,54 +1,50 @@
-using UnityEngine.InputSystem;
-using UnityEngine;
 using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 namespace MergeTest.Core
 {
 	public class PlayerInput : MonoBehaviour, IPlayerInputInfo
 	{
-		[SerializeField] private InputActionReference _clickAction;
-
 		public bool IsInputProcess { get; private set; }
 		public Vector2 PointerScreenPosition { get; private set; }
 
 		public event Action OnInputFinished;
 		public event Action OnInputStarted;
 
-		private InputAction ClickAction => _clickAction;
-
-		private void OnEnable()
-		{
-			ClickAction.started += OnPressStarted;
-			ClickAction.canceled += OnPressCanceled;
-			ClickAction.Enable();
-		}
-
-		private void OnDisable()
-		{
-			ClickAction.started -= OnPressStarted;
-			ClickAction.canceled -= OnPressCanceled;
-			ClickAction.Disable();
-		}
-
-		private void OnPressStarted(InputAction.CallbackContext context)
-		{
-			IsInputProcess = true;
-			UpdatePointerPosition();
-			OnInputStarted?.Invoke();
-		}
-
-		private void OnPressCanceled(InputAction.CallbackContext context)
-		{
-			IsInputProcess = false;
-			UpdatePointerPosition();
-			OnInputFinished?.Invoke();
-		}
-
-		private void UpdatePointerPosition() => PointerScreenPosition = Pointer.current.position.ReadValue();
-		
 		private void Update()
 		{
-			UpdatePointerPosition();
+			GetPointerInput(out var position, out var button);
+
+			if (button == null)
+				return;
+
+			PointerScreenPosition = position;
+
+			if (button.wasPressedThisFrame)
+			{
+				IsInputProcess = true;
+				OnInputStarted?.Invoke();
+			}
+
+			if (button.wasReleasedThisFrame)
+			{
+				IsInputProcess = false;
+				OnInputFinished?.Invoke();
+			}
+		}
+
+		private void GetPointerInput(out Vector2 position, out ButtonControl button)
+		{
+#if UNITY_EDITOR
+			button = Mouse.current.leftButton;
+			position = Mouse.current.position.ReadValue();
+#elif UNITY_ANDROID ||  UNITY_IOS
+				var touch = Touchscreen.current.primaryTouch;
+				button = touch.press;
+				position = touch.position.ReadValue();
+#endif
 		}
 	}
 }
